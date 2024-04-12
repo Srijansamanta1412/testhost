5a092ca3c56dbc0340ffa1bda35ba75f29f4f164
Srijan Samanta <srijansamanta1412@gmail.com>
2024-04-12 19:10:15 +0530

    Shruti Asshole

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-function Home(){
+function Home() {
     return (<div>
         Tetsing Home
         <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio asperiores fugiat repudiandae quia a nihil aperiam sit, nostrum debitis ipsam. Aspernatur exercitationem fuga natus cumque distinctio culpa quae eaque, hic nobis dolor dolore tempora! Vitae, provident.</div>
+        <div>SHruti Asshole</div>
     </div>)
 }
 export default Home;
